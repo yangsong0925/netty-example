97ac3234110c2f9fa5798226caf90b93a2297118
yangsong <yangsong1@bsoft.com.cn>
2020-11-12 00:07:11 +0800

    NIO TimeServer

---
+++ src/test/java/nioExample/time/TimeClient.java
@@ -0,0 +1,26 @@
+package nioExample.time;
+
+/**
+ * @Classname TimeClient
+ * @Description TODO
+ * @Date 2020/11/10 20:21
+ * @Created by ys
+ */
+public class TimeClient {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        new Thread(new TimeClientHandle("127.0.0.1", port), "TimeClient-001").start();
+    }
+
+}
---
+++ src/test/java/nioExample/time/TimeServer.java
@@ -0,0 +1,29 @@
+package nioExample.time;
+
+import java.io.IOException;
+
+/**
+ * @Classname TimeServer
+ * @Description TODO
+ * @Date 2020/11/10 20:21
+ * @Created by ys
+ */
+public class TimeServer {
+
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                // 采用默认值
+            }
+        }
+        MultiplexerTimeServer timeServer = new MultiplexerTimeServer(port);
+        new Thread(timeServer, "NIO-MultiplexerTimeServer-001").start();
+    }
+}
