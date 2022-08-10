090aeb2ff9ce5df810af9b66f41d33048a9f1ef2
yangsong <13980152803@163.com>
2022-08-10 15:57:45 +0800

    NIO test bytebuffer

---
+++ src/test/java/bytebuffer/nio/TestByteBuffer.java
@@ -0,0 +1,44 @@
+package bytebuffer.nio;
+
+import com.syys.util.ByteBufferUtil;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author yangsong
+ * @version 1.0
+ * @date 2022/8/10 15:44
+ */
+public class TestByteBuffer {
+
+    @Test
+    public void allocate() {
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        // 向buffer中写入1个字节的数据
+        buffer.put((byte) 97);
+        // 使用工具类，查看buffer状态
+        ByteBufferUtil.debugAll(buffer);
+
+        // 向buffer中写入4个字节的数据
+        buffer.put(new byte[]{98, 99, 100, 101});
+        ByteBufferUtil.debugAll(buffer);
+
+        // 获取数据
+        buffer.flip();
+        ByteBufferUtil.debugAll(buffer);
+        System.out.println(buffer.get());
+        System.out.println(buffer.get());
+        ByteBufferUtil.debugAll(buffer);
+
+        // 使用compact切换模式
+        buffer.compact();
+        ByteBufferUtil.debugAll(buffer);
+
+        // 再次写入
+        buffer.put((byte) 102);
+        buffer.put((byte) 103);
+        ByteBufferUtil.debugAll(buffer);
+    }
+
+}
