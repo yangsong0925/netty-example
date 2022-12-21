449274230291366477c0063ad32980d2ffc5db46
yangsong <13980152803@163.com>
2022-12-21 11:41:33 +0800

    实现自动重连机制

---
+++ src/main/java/uptime/UptimeServerHandler.java
@@ -0,0 +1,25 @@
+package uptime;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * @author yangsong
+ * @version 1.0
+ * @date 2022/12/21 11:01
+ */
+@ChannelHandler.Sharable
+public class UptimeServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    public void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // discard
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
